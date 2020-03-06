bc3deea370321a68c2c5bbc9947fd96c8274f808
zcq1933 <1933748107@qq.com>
2020-03-06 20:16:15 +0800

    123

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,17 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    uglify: {
-      release:{
-        files: {
-          'rectangle.min.js':'rectangle.js'
-        }
-      }       
-    }
+    imagemin: {
+      files:{
+        expand:true,
+        src:['images/*.{png,jpg,gif}'],
+        dest:'dist/'
+    
+      }
+      }
+
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['uglify:release']);
+  grunt.registerTask('default', ['imagemin']);
 };
